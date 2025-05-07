811602614046998f7ba0e6d1e4025d7584b974e5
koyoshi544 <koji2ky@gmail.com>
2025-05-07 17:34:17 +0900

    軽微な修正

--- upload_ws3.py
+++ upload_ws3.py
@@ -7,9 +7,9 @@ import boto3
 load_dotenv()
 
 # 設定値
-s3_file_path = 'R0704_student.pdf'
+s3_file_path = '20250423_kaigohoken.pdf'
 s3_bucket_name = 'ws3aclewm1il9xopvli7'
-local_file_path = '/tmp/R0704_student.pdf'
+local_file_path = '/tmp/20250423_介護保険認定結果通知書.pdf'
 content_type = 'application/pdf'
 
 # 実行内容
